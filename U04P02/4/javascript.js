bdf7d972cefa40d8f51461624fbfbd7e7975f197
ThiagoCingolani <thiagocingolani25@gmail.com>
2022-09-10 20:59:34 +0200

    Ejercicios

---
+++ U04P02/4/javascript.js
@@ -0,0 +1,28 @@
+let nombre;
+nombre = prompt("Ingrese su nombre: ");
+
+let apellido;
+apellido = prompt("Ingrese su apellido: ");
+
+let domicilio;
+domicilio = prompt("Ingrese su domicilio: ");
+
+let curso;
+curso = prompt("Ingrese su curso: ");
+
+let carrera;
+carrera = prompt("Ingrese su carrera: ");
+
+let materia;
+materia = prompt("Ingrese su materia: ");
+
+let nota_primer_parcial;
+nota_primer_parcial = prompt("Ingrese su nota en el primer parcial: ");
+
+document.write (nombre + "<br>");
+document.write (apellido + "<br>");
+document.write (domicilio + "<br>");
+document.write (curso + "<br>");
+document.write (carrera + "<br>");
+document.write (materia + "<br>");
+document.write (nota_primer_parcial + "<br>");
